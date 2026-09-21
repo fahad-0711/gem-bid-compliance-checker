5526cf1429edcfeec8f5e8ae4a6d62eca5f87f1e
fahad-0711 <g.fahad20057@gmail.com>
2026-09-21 15:38:09 +0530

    Clean up debug test scripts

--- test_msme3.py
+++
@@ -1,7 +0,0 @@
-from extraction.ocr_reader import extract_text_from_image
-from extraction.field_extractor import process_document
-
-text, conf = extract_text_from_image('data/sample_docs/IMG-Docs/p2/P2.jpeg')
-result = process_document('MSME.jpeg', text)
-print("Extracted udyam_number:", repr(result['fields'].get('udyam_number')))
-print("Full raw text:", repr(text))
--- test_pan3.py
+++
@@ -1,7 +0,0 @@
-from extraction.pdf_reader import extract_text_from_pdf
-from extraction.field_extractor import process_document
-
-text = extract_text_from_pdf('data\\sample_docs\\pdf-docs\\p2\\PAN_Card_02_Priya_Singh.pdf')
-result = process_document('PAN_Card_02_Priya_Singh.pdf', text)
-print("Extracted pan_number:", repr(result['fields'].get('pan_number')))
-print("Full raw text:", repr(text))
